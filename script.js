52cabe5cd8b31387db1f306a329c2e3dea9ab469
jtme <janis@tilla.me>
2014-06-29 21:28:18 +0300

    upd

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var fizzbuzzit = function(givennumber) {
         if (givennumber >= 1 && givennumber <= 100) {
                 // create a loop, fizz/buzz it
-                for (var n = 1; n <= 100; n++) {
+                for (var n = 1; n <= givennumber; n++) {
                     if (n % 15 === 0) {
                         addtolist("fizz-buzz");
                      } else if (n % 3 === 0){
